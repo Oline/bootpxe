f4af28f3218cd2573b9908f810e7003494a8f90d
Sylvain Leroy <sylvain@unmondelibre.fr>
2012-06-24 01:53:09 +0200

    Starting to split the huge makefile in smaller ones

--- Makefile
+++ Makefile
@@ -316,20 +316,6 @@ openbsd_doc:
 ################################# memtest86+ ###############################################
 ############################################################################################
 
-memtest: memtest_init memtest_get
-
-memtest_init:
-
-memtest_get: memtest_init
-	wget http://www.memtest.org/download/4.20/memtest86+-4.20.bin.gz -O $(DIR_TFTP_DISTRIB)/memtest86+-4.20.bin.gz
-	gunzip $(DIR_TFTP_DISTRIB)/memtest86+-4.20.bin.gz
-	mv $(DIR_TFTP_DISTRIB)/memtest86+-4.20.bin $(DIR_TFTP_DISTRIB)/memtest86+-4.20
-
-memtest_clean:
-	rm -f $(DIR_TFTP_DISTRIB)/memtest86+-4.20
-
-memtest_distclean: memtest_clean
-
-memtest_doc:
+-include memtest86.mk
 
 ############################################################################################
---
+++ memtest86.mk
@@ -0,0 +1,21 @@
+############################################################################################
+################################# memtest86+ ###############################################
+############################################################################################
+
+memtest: memtest_init memtest_get
+
+memtest_init:
+
+memtest_get: memtest_init
+	wget http://www.memtest.org/download/4.20/memtest86+-4.20.bin.gz -O $(DIR_TFTP_DISTRIB)/memtest86+-4.20.bin.gz
+	gunzip $(DIR_TFTP_DISTRIB)/memtest86+-4.20.bin.gz
+	mv $(DIR_TFTP_DISTRIB)/memtest86+-4.20.bin $(DIR_TFTP_DISTRIB)/memtest86+-4.20
+
+memtest_clean:
+	rm -f $(DIR_TFTP_DISTRIB)/memtest86+-4.20
+
+memtest_distclean: memtest_clean
+
+memtest_doc:
+
+############################################################################################
