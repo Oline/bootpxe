0dbbffb5fe7fd9420fd50e825264642dcf6d19f4
Sylvain Leroy <sylvain@unmondelibre.fr>
2012-10-03 22:20:17 +0200

    Adding an install target for memtest made to do the stuff after getting the binary from outside

--- memtest86.mk
+++ memtest86.mk
@@ -5,9 +5,12 @@
 memtest: memtest_init memtest_get
 
 memtest_init:
+	mkdir -p $(DIR_TFTP_DISTRIB)
 
 memtest_get: memtest_init
 	wget http://www.memtest.org/download/4.20/memtest86+-4.20.bin.gz -O $(DIR_TFTP_DISTRIB)/memtest86+-4.20.bin.gz
+
+memtest_install: memtest_get
 	gunzip $(DIR_TFTP_DISTRIB)/memtest86+-4.20.bin.gz
 	mv $(DIR_TFTP_DISTRIB)/memtest86+-4.20.bin $(DIR_TFTP_DISTRIB)/memtest86+-4.20
 
